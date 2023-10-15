4700ea8a4bdc6a4a6975230d2865e6ade3395561
sferna10 <sferna10@email.cpcc.edu>
2023-10-14 21:22:37 -0400

    creating index.js, generateMarkdown and addig gitignore file

---
+++ Utils/generateMarkdown.js
@@ -0,0 +1,74 @@
+function renderLicenseBadge(license){
+    if (license !== 'None'){
+        return '![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)';
+    }
+return '';
+}
+
+function renderLicenseLin(license){
+    if (license !== "None"){
+        return '\n* [License](#license)\n';
+    }
+    return '';
+}
+
+function renderLicenseSection(license){
+    if (license !== 'None'){
+        return '## License This project is licensed under the $(license) license.';
+    }
+return '';
+}
+
+function generateMarkdown(data) {
+    return '# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+*[Usage](#usage)
+${renderLicenseLink(data.license)}
+*[Contributing](#contributing)
+*[Tests](#test)
+*[Questions](#questions)
+
+##Installation
+
+To install necessary dependencies, run the following command:
+
+\'\'\'
+
+${data.installation}
+
+\'\'\'
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+\'\'\'
+
+${data.test}
+\'\'\'
+
+##Questions
+
+If you have any questions about the reportError. open an issue or contant me  directly at ${
+    data.email
+}. You can find more of my work at [${data.github}](https://github.com/${data.github}/),
+
+;
+}
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs;');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./Utils/generateMarkdown');
+
+const promptUser = () => {
+    return inquirer.prompt([
+{
+        Type: 'input', 
+        name: 'github', 
+        message: 'What is your GitHub username?',
+},
+{
+    Type: 'input', 
+    name:  'email',
+    message:'What is your email address?',
+},
+{
+    Type: 'input', 
+    name: 'title',
+    message: "What is your project's name",
+},
+{
+    Type: 'input', 
+    name: 'description',
+    message: 'Please write a short description of your project',
+},
+{
+    Type: 'list', 
+    name: 'license',
+    message: 'What kind of license should your project have?',
+choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+},
+{
+    Type: 'input', 
+    name:'Installation',
+    message: 'What command should be sun to install debendencies',
+    default: 'npm i',
+},
+{
+    Type: 'input', 
+    name: 'test',
+    message: "What command should be run to run tests?",
+    default: 'npm tests',
+}, 
+ {
+    Type: 'input', 
+    name:'usage',
+    message: 'What does the user need to know about using the repo?',
+},
+    ]);
+}
+function writeTofile(fileName,data) {
+    returnfs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('generating README...');
+        writeTofile('README.md', generateMarkdown({ ...inquirerResponses}));
+});
+}
+
+init();
